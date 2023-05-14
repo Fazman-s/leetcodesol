10e6e7479ea29252c8e4aaac4f00c58480cdc0a7
Fazman S <fazmansam@gmail.com>
2023-05-14 23:36:15 +0530

    Time: 9 ms (99.39%) | Memory: 41.7 MB (87.83%) - LeetSync

---
+++ palindrome-number/palindrome-number.java
@@ -0,0 +1,40 @@
+// class Solution {
+//     public boolean isPalindrome(int x) {
+//         int ans=0;
+//         int rem;
+//         int ld;
+//         while(x>0){
+//            ld= x%10 ;  //last digit 232%10 = 2(last dogit))
+//            ans=(ans*10)+ld; // (0*10)+2 =2 
+//             x=x/10;    //232/10 == 23 now x=23 n same procedure follows   
+//         }
+//         if (x == ans){
+//           return true;
+//         }else{
+//           return false;
+//         }
+        
+
+//     }
+// }
+
+class Solution {
+    public boolean isPalindrome(int x) {
+      if(x < 0){
+}
+
+        int reverse = 0;
+        int num=x;
+
+
+        while(x > 0){
+            reverse = reverse*10 + x%10;
+            x = x/10;
+        }
+
+        if(reverse == num)
+          return true;
+
+        return false;
+    }
+}
