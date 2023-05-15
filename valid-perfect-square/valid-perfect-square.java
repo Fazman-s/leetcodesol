f6392811163a6e6f355a6ac15f6b4f20cbc7ec15
Fazman S <fazmansam@gmail.com>
2023-05-15 22:37:43 +0530

    Time: 0 ms (100.00%) | Memory: 38.9 MB (98.04%) - LeetSync

---
+++ valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long start=0;
+        long end=num;
+        while(start<=end){
+            long mid=start +(end-start)/2;
+            if(mid*mid == num){
+                return true;
+            }
+            if(mid*mid > num){
+                end=mid-1;
+            }else {
+                start=mid+1;
+            }
+        }
+        return false;
+       
+
+
+   
+}
+}
