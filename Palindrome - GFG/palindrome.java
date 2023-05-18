e9b499fb9b00ef5a93f4ff336fd6f9271460b95d
Fazman S <fazmansam@gmail.com>
2023-05-18 19:32:02 +0530

    Added solution - LeetHub

---
+++ Palindrome - GFG/palindrome.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            String ans = ob.is_palindrome(n);
+            System.out.println(ans);
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public String is_palindrome(int n)
+    {
+     int x=n;
+       int ans=0;
+       while(n>0){
+       int ld=n%10;
+       ans=ans*10 + ld;
+       n=n/10;
+       
+       }
+       if(ans == x) return "Yes";
+       return "No";
+       
+       
+
+}
+}
