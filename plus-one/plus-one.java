b5561681a509cdf5cb6239083b2a4a4c79d968c1
Fazman S <fazmansam@gmail.com>
2023-05-20 21:46:39 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (74.37%) - LeetSync

---
+++ plus-one/plus-one.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+}
+
+// 3=3
+// 2*10+3
+// 1*100+23
+
+// 0*10
