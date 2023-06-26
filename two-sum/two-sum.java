460644cee43972e64b499a7f9c71df06f00912c7
Fazman S <fazmansam@gmail.com>
2023-06-26 23:50:39 +0530

    Time: 1 ms (99.38%) | Memory: 43.7 MB (49.63%) - LeetSync

--- two-sum/two-sum.java
+++ two-sum/two-sum.java
@@ -3,9 +3,9 @@ class Solution {
         int[] res=new int[2];
         HashMap<Integer,Integer> map= new HashMap<Integer,Integer>();
         for(int i=0;i<nums.length;i++){
-            if(map.containsKey(target-nums[i])){
-                res[1]=i;
-                res[0]=map.get(target-nums[i]);
+            if(map.containsKey(target-nums[i])){  //9-2 =7 exist in nums 
+                res[1]=i;  // index 1
+                res[0]=map.get(target-nums[i]); // index at 0
                 return res;
             }
             map.put(nums[i],i);
