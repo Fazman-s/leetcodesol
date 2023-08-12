92a36dff473f3503adf464c2e054c12d5e614bf8
Fazman S <fazmansam@gmail.com>
2023-08-12 18:44:51 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (97.07%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -14,7 +14,7 @@ class Solution {
             }
 
         }
-        return start;
+        return start;  /* If at some point we have our starting index greater than ending index we return the starting index , which indicates that array has no target and start indicates the position at which it need to be inserted.*/
 
     }
 }
