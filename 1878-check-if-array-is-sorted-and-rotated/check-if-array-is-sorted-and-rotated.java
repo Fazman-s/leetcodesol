0cadb105fbea82200bc4f384749fd6cfffc24b10
Fazman S <fazmansam@gmail.com>
2023-08-19 18:27:00 +0530

    Time: 0 ms (100.00%) | Memory: 39.8 MB (94.28%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -11,27 +11,28 @@
         }
         return true;
     }
-}*/
+}
+or*/
 
 class Solution {
-            public boolean check(int[] A) {
-                    int k = 0, n = A.length;
-                    boolean flag=false;
+            public boolean check(int[] nums) {
+                   int k=0;
+                   int n=nums.length;
+                   boolean flag=false;
 
-                    if(A[0] >= A[n-1]){
-                        flag = true;
+                   if(nums[0] >= nums[n-1]){
+                       flag = true;
+                   }
 
-                    }
+                   for(int i=0;i<n-1;i++){
+                       if(nums[i] > nums[i+1]){
+                           k++;
+                       }
+                   }
 
-                    for(int i=0;i<n-1;i++){
-                        if(A[i]>A[i+1]){
-                            k++;
-                        }
-                    }
-
-                    if(flag ==true && k==1 || k==0){
-                        return true;
-                    }
-                    return false;
+                   if(flag == true && k==1 || k==0){
+                       return true;
+                   }
+                   return false;
 }
 }
