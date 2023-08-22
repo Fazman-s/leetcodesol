68f63656813eb7d039015c04f94bac2a7ddd060f
Fazman S <fazmansam@gmail.com>
2023-08-22 21:59:58 +0530

    Time: 4 ms (81.47%) | Memory: 41.6 MB (97.34%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -2,14 +2,14 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length()) return false;
 
-        int[] char_count = new int[26];
+        int[] char_count = new int[26];   //26 letters in alphabets
         for(int i=0;i<s.length();i++){
-            char_count[s.charAt(i)-'a']++;
-            char_count[t.charAt(i)-'a']--;
+            char_count[s.charAt(i)-'a']++;    // subtract each element from ascii character of and increment the count by one
+            char_count[t.charAt(i)-'a']--;    //do the same as above but decrement the count by one
         }
 
-        for(int count:char_count){
-            if(count != 0){
+        for(int count:char_count){          
+            if(count != 0){             //if count = 0 -- only then it is an anagram
                 return false;
             }
         }
