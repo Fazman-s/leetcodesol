1162825a085ea3547f5b049180d3c15efca3d17b
Fazman S <fazmansam@gmail.com>
2023-08-22 21:47:32 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (98.69%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
@@ -3,9 +3,9 @@ class Solution {
         int n=mat.length;
         int sum=0;
         for(int i=0;i<n;i++){
-            sum+= mat[i][i] + mat[i][n-i-1];
+            sum+= mat[i][i] + mat[i][n-1 -i];  //diagnal ele indexes ii -11,22,33(primary diag),[i][n-1-i]-second diag 
             }
-            if(n%2!=0){
+            if(n%2!=0){       //if n is odd the minus the midle ele from sum
              sum -= mat[n/2][n/2];
             }
 
