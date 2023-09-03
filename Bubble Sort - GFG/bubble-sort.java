d78f558b9829d6e7b117a6e4a909327996d218f6
Fazman S <fazmansam@gmail.com>
2023-09-03 22:38:07 +0530

    Added solution - LeetHub

---
+++ Bubble Sort - GFG/bubble-sort.java
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution
+{
+    //Function to sort the array using bubble sort algorithm.
+	public static void bubbleSort(int nums[], int n)
+    {
+        boolean swapped;
+        for(int i=0;i<n;i++){
+            swapped = false;
+            for(int j=1;j<n-i;j++){
+                if(nums[j]<nums[j-1]){
+                    int temp=nums[j];
+                    nums[j]=nums[j-1];
+                    nums[j-1]=temp;
+                    swapped = true;
+                }
+
+            }
+            if(!swapped){
+                break;
+            }
+        }
+    }
+}
+
+//{ Driver Code Starts.
+
+class Sorting
+{
+    //method to print the Elements of the array
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+	public static void main(String args[])
+	{
+	    //taking input using Scanner class
+		Scanner sc = new Scanner(System.in);
+		
+		//taking total testcases
+		int t = sc.nextInt();
+		while(t>0)
+		{
+		    //taking total elements
+			int n = sc.nextInt();
+			
+			//creating a new array of length n
+			int arr[] = new int[n];
+			
+			//inserting elements to the array
+			for(int i=0;i<n;i++)
+			{
+				arr[i] = sc.nextInt(); 
+			}
+			
+			
+			//calling bubbleSort() method
+			new Solution().bubbleSort(arr,n);
+			
+			//calling printArray() method
+			printArray(arr);
+			
+		t--;	
+		}
+	}
+
+}
+// } Driver Code Ends
