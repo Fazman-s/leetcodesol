b98d59be002ee773a81c055f9b77d13bc7a3824f
Fazman S <fazmansam@gmail.com>
2023-09-11 19:33:02 +0530

    Time: 1 ms (91.17%) | Memory: 39.6 MB (65.21%) - LeetSync

---
+++ 258-add-digits/add-digits.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int addDigits(int num) {
+
+        if(num==0) return 0;
+        else if(num<=9){
+            return num;
+        }
+        else{
+            int s=0;
+            while(num>0){
+                int ld=num%10;
+                s=s+ld;
+                num=num/10;
+            }
+            return addDigits(s);
+        }
+    }
+}
