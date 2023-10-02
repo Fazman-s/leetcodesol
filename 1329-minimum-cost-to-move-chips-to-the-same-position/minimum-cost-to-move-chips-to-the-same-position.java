0778b73b7edd28a1dcbe0fabe77abe37f61d2633
Fazman S <fazmansam@gmail.com>
2023-10-02 19:23:37 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (51.63%) - LeetSync

--- 1329-minimum-cost-to-move-chips-to-the-same-position/minimum-cost-to-move-chips-to-the-same-position.java
+++ 1329-minimum-cost-to-move-chips-to-the-same-position/minimum-cost-to-move-chips-to-the-same-position.java
@@ -22,4 +22,7 @@ class Solution {
 each and every chips at odd position can be taken to position 1 at 0 cost and each and every chips at even position can be taken to position 2 at 0 cost.
 we have all the chips at position 1 and 2 only. And now we have to decide whether to take all the chips from position 2 to 1 or position 1 to 2, and in both the cases cost will be 1 per chips.
 so better will be to pick minimum of the two and put it to the other index to have minimum cost
+
+solution :
+Count the number of odd and even positions and minimum of the two will be answer.
 */
