3e7c7f8e0fa1fed5897406ea84ad470b5c702116
Fazman S <fazmansam@gmail.com>
2023-10-03 20:46:59 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (99.49%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,5 +1,33 @@
 class Solution {
     public void sortColors(int[] nums) {
+        //optimized solution - Dutch national Falg algorithm -O(n) 
+        int n=nums.length;
+        int low=0, mid=0, high=n-1;
+        while(mid <= high){
+            if(nums[mid]==0){
+               swap(nums,low,mid);
+                low++;
+                mid++;
+            }else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums,mid,high);
+                high--;
+            }
+        }
+
+    }
+
+    static void swap(int[] nums,int i,int j){
+        int temp = nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+}
+
+/* 
+using bubble sort - time comp-- O(N^2)
         int n=nums.length;
         boolean swapped;
         for(int i=0;i<n;i++){
@@ -17,5 +45,4 @@ class Solution {
                 break;
             }
         }
-    }
-}
+*/        
