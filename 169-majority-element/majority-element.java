5b72f049e4068d1662198a0baee7d1d833f9b870
Fazman S <fazmansam@gmail.com>
2023-10-06 20:38:32 +0530

    Time: 11 ms (35.07%) | Memory: 46.4 MB (91.43%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,6 +1,23 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        // Base case...
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int value = map.getOrDefault(nums[i], 0);
+            map.put(nums[i], value + 1);
+        }
+        //searching for the majority element:
+        for (Map.Entry<Integer, Integer> it : map.entrySet()) {
+            if (it.getValue() > (nums.length / 2)) {
+                return it.getKey();
+            }
+        }
+
+        return -1;
+    }
+       
+}
+/*
+ // Base case...
         if (nums.length == 1) {
 		    return nums[0];
 	    }
@@ -10,5 +27,4 @@ class Solution {
         // Since the majority element appears more than n / 2 times...
         // The n/2 -th element in the sorted nums must be the majority element...
 	    return nums[nums.length / 2];
-    }
-}
+*/        
