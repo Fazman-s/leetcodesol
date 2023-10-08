f59b4c61b8b458bbb129736767a9f3650e189cd2
Fazman S <fazmansam@gmail.com>
2023-10-08 19:26:53 +0530

    Time: 2 ms (75.52%) | Memory: 60.6 MB (83.27%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,18 +1,28 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int lsf = Integer.MAX_VALUE; // least so far
-        int op = 0; // overall profit
-        int pist = 0; // profit if sold today
+        // int lsf = Integer.MAX_VALUE; // least so far
+        // int op = 0; // overall profit
+        // int pist = 0; // profit if sold today
         
-        for(int i = 0; i < prices.length; i++){
-            if(prices[i] < lsf){ // if we found new buy value which is more smaller then previous one
-                lsf = prices[i]; // update our least so far
-            }
-            pist = prices[i] - lsf; // calculating profit if sold today by, Buy - sell
-            if(op < pist){ // if pist is more then our previous overall profit
-                op = pist; // update overall profit
-            }
+        // for(int i = 0; i < prices.length; i++){
+        //     if(prices[i] < lsf){ // if we found new buy value which is more smaller then previous one
+        //         lsf = prices[i]; // update our least so far
+        //     }
+        //     pist = prices[i] - lsf; // calculating profit if sold today by, Buy - sell
+        //     if(op < pist){ // if pist is more then our previous overall profit
+        //         op = pist; // update overall profit
+        //     }
+        // }
+        // return op; // return op 
+        int mini=prices[0];
+        int maxprofit=0;
+        int n=prices.length;
+        
+        for(int i=1;i<n;i++){
+            int cost = prices[i] - mini;
+            maxprofit = Math.max(maxprofit,cost);
+            mini= Math.min(mini,prices[i]);
         }
-        return op; // return op 
+        return maxprofit;
     }
 }
