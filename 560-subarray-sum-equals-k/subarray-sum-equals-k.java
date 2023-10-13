8123101736683635e8bd290eadd025b18c75d31c
Fazman S <fazmansam@gmail.com>
2023-10-13 19:43:43 +0530

    Time: 23 ms (64.86%) | Memory: 46.5 MB (34.60%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,17 +1,36 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int n=nums.length;
-        int count=0;
-
-        for(int i=0;i<n;i++){
-            int sum=0;
-            for(int j=i;j<n;j++){
-                sum+=nums[j];
-                if(sum==k){
-                    count++;
-                }
+        //Optimal - prefix sum method
+        int preSum = 0;
+        int count = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        for(int i=0;i<nums.length;i++){
+            preSum += nums[i];
+            if(map.containsKey(preSum -k)){
+                count += map.get(preSum-k);
             }
+            map.put(preSum,map.getOrDefault(preSum,0)+1);
         }
         return count;
+
     }
 }
+
+
+
+        //brute force TC=O(n^2)
+
+        // int n=nums.length;
+        // int count=0;
+
+        // for(int i=0;i<n;i++){
+        //     int sum=0;
+        //     for(int j=i;j<n;j++){
+        //         sum+=nums[j];
+        //         if(sum==k){
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
