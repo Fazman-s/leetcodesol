3e82a7f6037616178557caae605b0b72d48fb197
Fazman S <fazmansam@gmail.com>
2023-10-14 21:25:34 +0530

    Time: 0 ms (100.00%) | Memory: 56.1 MB (19.92%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,5 +1,33 @@
 class Solution {
     public void rotate(int[] nums, int k) {
+        int n=nums.length;
+        k=k%n;
+       // Reverse entire array
+        reverse(nums, 0, n - 1);
+        // Reverse first k elements
+        reverse(nums, 0, k - 1);
+        // Reverse remaining elements
+        reverse(nums, k, n - 1);
+    }
+
+    public static void reverse(int nums[], int start, int end){
+    // While start index is less than end index
+     while(start < end){
+        // Swap elements at start and end indices
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        // Increment start index and decrement end index
+        start++;
+        end--;
+     }
+    }
+
+
+}
+
+
+/*  brute force
     int n=nums.length;
     if (n == 0)
       return;
@@ -18,7 +46,4 @@ class Solution {
       nums[i] = temp[i];
     }
 
-
-
-    }
-}
+   */ 
