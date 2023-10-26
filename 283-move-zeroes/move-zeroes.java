d0da9bfb66e3cb1237d2872e8b5a0f2916f9c547
Fazman S <fazmansam@gmail.com>
2023-10-26 20:45:46 +0530

    Time: 1 ms (100.00%) | Memory: 45.2 MB (22.03%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] arr) {
+        int n=arr.length;
+        int j  = 0;
+        for(int i =0;i<n;i++){
+            if(arr[i] != 0){
+                arr[j] = arr[i];
+                j++;
+            }
+        }
+        while(j<n) arr[j++] = 0;
+    }
+}
+
