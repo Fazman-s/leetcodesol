60c26884071e2a70c0146b7942bf5c17e990b1e6
Fazman S <fazmansam@gmail.com>
2023-11-22 21:08:27 +0530

    Time: 8 ms (59.37%) | Memory: 42.1 MB (75.35%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -2,30 +2,26 @@ class Solution {
     public String reverseWords(String s) {
         String[] words=s.trim().split(" +");   //trims begining and end spaces -- splits trimmed words into array of words
                                             //based on spaces btw -" +"  --one or more spaces 
-        StringBuilder sb=new StringBuilder();
-        for(int i=words.length-1;i>=0;i--){
-            sb.append(words[i]);
-            if(i != 0){
-                sb.append(" ");  // adds space after each word except the the last(in this case i=0)
-            }
+        // StringBuilder sb=new StringBuilder();
+        // for(int i=words.length-1;i>=0;i--){
+        //     sb.append(words[i]);
+        //     if(i != 0){
+        //         sb.append(" ");  // adds space after each word except the the last(in this case i=0)
+        //     }
             
+        // }
+        // return sb.toString();
+
+        int i=0,j=words.length-1;
+        while(i<j) {
+            String t = words[i];
+            words[i] = words[j];
+            words[j] = t;
+            i++;
+            j--;
         }
-        return sb.toString();
+        return String.join(" ", words);
 
-
-        /*
-         // Initialize the output string
-        String out = "";
-
-        // Iterate through the words in reverse order
-        for (int i = words.length - 1; i > 0; i--) {
-            // Append the current word and a space to the output
-            out += words[i] + " ";
-        }
-
-        // Append the first word to the output (without trailing space)
-        return out + words[0];
-        */
 
     }
 }
